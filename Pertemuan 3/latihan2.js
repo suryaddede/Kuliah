ee7e5d6873ec35bde7a9d14f6ee48e114aa6ba26
Muhammad Surya Adhi Setiawan <21082010167@student.upnjatim.ac.id>
2023-03-02 14:33:18 +0700

    Modify and add comment

--- Pertemuan 3/latihan2.js
+++ Pertemuan 3/latihan2.js
@@ -1,20 +1,35 @@
 function tambah() {
+    // Mendapatkan nilai dari input dengan id "bil1"
     let bil1 = document.getElementById("bil1").value;
+    // Mendapatkan nilai dari input dengan id "bil2"
     let bil2 = document.getElementById("bil2").value;
+    // Menampilkan hasil penjumlahan dari kedua bilangan
     document.getElementById("hasil").innerHTML = parseFloat(bil1)+parseFloat(bil2);
 }
+
 function kurang() {
+    // Mendapatkan nilai dari input dengan id "bil1"
     let bil1 = document.getElementById("bil1").value;
+    // Mendapatkan nilai dari input dengan id "bil2"
     let bil2 = document.getElementById("bil2").value;
+    // Menampilkan hasil pengurangan dari kedua bilangan
     document.getElementById("hasil").innerHTML = parseFloat(bil1)-parseFloat(bil2);
 }
+
 function kali() {
+    // Mendapatkan nilai dari input dengan id "bil1"
     let bil1 = document.getElementById("bil1").value;
+    // Mendapatkan nilai dari input dengan id "bil2"
     let bil2 = document.getElementById("bil2").value;
+    // Menampilkan hasil perkalian dari kedua bilangan
     document.getElementById("hasil").innerHTML = parseFloat(bil1)*parseFloat(bil2);
 }
+
 function bagi() {
+    // Mendapatkan nilai dari input dengan id "bil1"
     let bil1 = document.getElementById("bil1").value;
+    // Mendapatkan nilai dari input dengan id "bil2"
     let bil2 = document.getElementById("bil2").value;
+    // Menampilkan hasil pembagian dari kedua bilangan
     document.getElementById("hasil").innerHTML = parseFloat(bil1)/parseFloat(bil2);
 }
