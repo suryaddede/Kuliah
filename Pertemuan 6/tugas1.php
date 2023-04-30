0cad7628d78053795963ddb189edf810ab36cdbd
Muhammad Surya Adhi Setiawan <21082010167@student.upnjatim.ac.id>
2023-04-30 21:47:43 +0700

    Add files

--- Pertemuan 6/tugas1.php
+++ Pertemuan 6/tugas1.php
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 <?php
 if (isset($_POST["submit"])) {
     if (empty($_POST["email"]) || empty($_POST["password"])) {
@@ -53,60 +52,4 @@ if (isset($_POST["submit"])) {
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/js/bootstrap.min.js" integrity="sha384-+sLIOodYLS7CIrQpBjl+C7nPvqq+FbNUBDunl/OZv93DB7Ln/533i8e/mZXLi/P+" crossorigin="anonymous"></script>
 </body>
 
-=======
-<?php
-if (isset($_POST["submit"])) {
-    if (empty($_POST["email"]) || empty($_POST["password"])) {
-        header("Location: tugas2_error.php");
-        exit();
-    } else {
-        $email = $_POST["email"];
-        header("Location: tugas1_sukses.php?email=$email");
-        exit();
-    }
-}
-?>
-
-<!DOCTYPE html>
-<html>
-
-<head>
-    <title>Form Login</title>
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/css/bootstrap.min.css" integrity="sha384-xOolHFLEh07PJGoPkLv1IbcEPTNtaed2xpHsD9ESMhqIYd0nLMwNLD69Npy4HI+N" crossorigin="anonymous">
-    <style>
-        .container {
-            height: 100vh;
-            width: 40vw;
-        }
-    </style>
-</head>
-
-<body class="container d-flex align-items-center bg-dark text-light">
-    <div class="row justify-content-center card border-light bg-dark w-100">
-        <div class="col-12 bg-dark border-light text-center card-header">
-            <h1>Login</h1>
-        </div>
-        <div class="card-body px-5">
-            <form action="tugas1.php" method="post">
-                <div class="form-group">
-                    <label for="email" class="font-weight-bold card-text">Email</label>
-                    <input type="text" class="form-control" name="email" id="email" require>
-                </div>
-                <div class="form-group">
-                    <label for="password" class="font-weight-bold card-text">Password</label>
-                    <input type="password" class="form-control" name="password" id="password" require minlength="8">
-                </div>
-                <div class="text-center">
-                    <input type="submit" class="btn btn-primary w-25" name="submit" value="Login">
-                </div>
-            </form>
-        </div>
-    </div>
-
-    <script src="https://cdn.jsdelivr.net/npm/jquery@3.5.1/dist/jquery.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
-    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js" integrity="sha384-9/reFTGAW83EW2RDu2S0VKaIzap3H66lZH81PoYlFhbGU+6BZp6G7niu735Sk7lN" crossorigin="anonymous"></script>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/js/bootstrap.min.js" integrity="sha384-+sLIOodYLS7CIrQpBjl+C7nPvqq+FbNUBDunl/OZv93DB7Ln/533i8e/mZXLi/P+" crossorigin="anonymous"></script>
-</body>
-
->>>>>>> 269ff34217e6a05d29e0405f1d907227624baef5
 </html>
--- Pertemuan 6/tugas3_biodata.php
+++ Pertemuan 6/tugas3_biodata.php
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 <?php
 $nama = array("Citra Dewi", "Eka Putri", "Dewi Lestari", "Agus Setiawan", "Gita Permata", "Indah Puspita", "Budi Santoso", "Vina Amelia", "Hendra Pratama", "Joko Susilo");
 $start = 1192;
@@ -45,52 +44,4 @@ foreach ($nama as $n) {
             </div>
         </div>
     </div>
-=======
-<?php
-$nama = array("Citra Dewi", "Eka Putri", "Dewi Lestari", "Agus Setiawan", "Gita Permata", "Indah Puspita", "Budi Santoso", "Vina Amelia", "Hendra Pratama", "Joko Susilo");
-$start = 1192;
-$kota = array("Jakarta", "Gresik", "Surabaya", "Sidoarjo", "Mojokerto", "Jombang", "Bangkalan", "Lamongan", "Malang", "Kediri", "Madiun");
-$posisi = array("Web Developer", "Software Engineer", "Data Analyst", "Network Administrator", "IT Project Manager", "Database Administrator", "Cybersecurity Analyst", "IT Support Specialist", "Cloud Architect", "UI/UX Designer", "Artificial Intelligence Engineer");
-$perusahaan = array("Garuda Indonesia", "Telkom Indonesia", "Bank Mandiri", "Bank Central Asia", "Bank Rakyat Indonesia", "Traveloka", "Unilever Indonesia", "Gojek", "Tokopedia", "Shopee Indonesia", "Grab Indonesia");
-
-foreach ($nama as $n) {
-    $email = strtolower(str_replace(" ", "", $n)) . "@gmail.com";
-    $telepon = "0812" . rand(10000000, 99999999);
-?>
-    <div class="card bg-dark border my-5">
-        <div class="row no-gutters">
-            <div class="col-4">
-                <img src="https://faces-img.xcdn.link/image-lorem-face-<?php echo $start++; ?>.jpg" class="card-img" alt="Profile">
-            </div>
-            <div class="col-8">
-                <div class="card-body font1">
-                    <div class="py-3">
-                        <p class="card-text ml-3">Nama&nbsp;&nbsp;: <?php echo $n; ?></p>
-                        <p class="card-text ml-3">Email&nbsp;: <?php echo $email; ?></p>
-                        <p class="card-text ml-3">Nomor&nbsp;: <?php echo $telepon; ?></p>
-                    </div>
-                    <hr class="border my-3">
-                    <div class="row">
-                        <div class="col-5 pt-2">
-                            <h5 class="card-title font2"> Pendidikan</h6>
-                                <ul>
-                                    <li class="card-text">SD Negeri <?php echo mt_rand(1, 10) . " " . $kota[mt_rand(1, 10)]; ?></li>
-                                    <li class="card-text">SMP Negeri <?php echo mt_rand(1, 10) . " " . $kota[mt_rand(1, 10)]; ?></li>
-                                    <li class="card-text">SMA Negeri <?php echo mt_rand(1, 10) . " " . $kota[mt_rand(1, 10)]; ?></li>
-                                    <li class="card-text">UPN "Veteran" Jatim</li>
-                                </ul>
-                        </div>
-                        <div class="col-7 pt-2">
-                            <h5 class="card-title font2"> Pengalaman</h5>
-                            <ul>
-                                <li class="card-text"><?php echo mt_rand(1, 3) . " tahun " . $posisi[mt_rand(1, 10)] . " " . $perusahaan[mt_rand(1, 10)]; ?></li>
-                                <li class="card-text"><?php echo mt_rand(4, 7) . " tahun " . $posisi[mt_rand(1, 10)] . " " . $perusahaan[mt_rand(1, 10)]; ?></li>
-                            </ul>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div>
-    </div>
->>>>>>> 269ff34217e6a05d29e0405f1d907227624baef5
 <?php } ?>
